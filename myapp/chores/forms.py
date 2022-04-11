c9bf351808c51de24abe8c29fa6830ed22415a16
Claire Carden <smclairecarden@gmail.com>
2022-04-11 14:51:07 -0500

    change description field to StringField

--- myapp/chores/forms.py
+++ myapp/chores/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, BooleanField
+from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 class ChoreForm(FlaskForm):
     chore = StringField('Chore', validators=[DataRequired()])
-    description = TextAreaField('Description')
+    description = StringField('Description')
     completed_by = StringField('Completed_By')
     done = BooleanField('Done')
     submit = SubmitField('Post')
