56817b208b692b701fa840244cb7c44802df2fab
Claire Carden <smclairecarden@gmail.com>
2022-04-12 13:29:59 -0500

    add image to register form

--- myapp/chores/forms.py
+++ myapp/chores/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 class ChoreForm(FlaskForm):
     chore = StringField('Chore', validators=[DataRequired()])
     description = StringField('Description')
-    completed_by = StringField('Completed_By')
+    completed_by = StringField('Completed By')
     done = BooleanField('Done')
     submit = SubmitField('Post')
     
